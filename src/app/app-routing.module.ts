c382d2ed9f4bca3fa6d42fc51b4d175afd861854
Lukas Momot <lukasz.momot@hotmail.com>
2019-05-16 13:20:25 +0200

    search page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { SearchPageComponent } from './search-page/search-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'search-page', pathMatch: 'full'},
+  { path: 'search-page', component: SearchPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
