953218e40e5132ebc1de55003cf914184faaeefd
Lukas Momot <lukasz.momot@hotmail.com>
2019-09-20 11:39:05 +0200

    Add support of cloud for production build

--- src/app/shared/stocks.service.ts
+++ src/app/shared/stocks.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -9,7 +10,9 @@ import { Stock } from './models/stock';
 })
 export class StocksService {
 
-  private url = 'http://localhost:5001';
+  // private url = 'http://localhost:5001';
+
+  private url = environment.backendUrl;
 
   constructor(private httpClient: HttpClient) { }
 
